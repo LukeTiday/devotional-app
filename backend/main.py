f0713dd29fb624a342a33a5176f652588ed93db7
Luke Tiday <luketiday@gmail.com>
2026-06-04 16:54:07 -0400

    Fix Cors 2

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "devotional-app-frontend-production.up.railway.app",
+        "https://devotional-app-frontend-production.up.railway.app",
     ]
     allow_credentials=True,
     allow_methods=["*"],
