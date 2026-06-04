785300e058ebc11819adbbd00d4197f3b4d954ad
Luke Tiday <luketiday@gmail.com>
2026-06-04 16:59:00 -0400

    Fix Cors 4

--- backend/main.py
+++ backend/main.py
@@ -12,8 +12,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://devotional-app-frontend-production.up.railway.app"
-    ]
+        "https://devotional-app-frontend-production.up.railway.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
